0154b93ba9e98913f21c3c61dd3f1fb142f623b6
Noemi Valin <noemivalin@outlook.fr>
2020-11-11 15:57:55 +0100

    restaurant app ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{ name: 'Yummy', address: "rue oberkampf", category: "italian" }, { name: 'La tour dargent', address: "quai de seine", category: "french" }])
